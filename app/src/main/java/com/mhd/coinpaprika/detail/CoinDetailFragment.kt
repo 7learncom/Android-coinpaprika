0e857a673074d79465828d6bc078f718dddd13d8
Mohammad Hasan Dabbaghy <m.dabbaghy@gmail.com>
2024-03-09 16:18:35 +0330

    Config nav graph

---
+++ app/src/main/java/com/mhd/coinpaprika/detail/CoinDetailFragment.kt
@@ -0,0 +1,12 @@
+package com.mhd.coinpaprika.detail
+
+import androidx.fragment.app.Fragment
+import com.mhd.coinpaprika.R
+import com.mhd.coinpaprika.databinding.FragmentDetailBinding
+
+class CoinDetailFragment : Fragment(R.layout.fragment_detail) {
+
+    private var _binding: FragmentDetailBinding? = null
+    private val binding: FragmentDetailBinding
+        get() = _binding!!
+}
---
+++ app/src/main/java/com/mhd/coinpaprika/list/CoinListFragment.kt
@@ -0,0 +1,12 @@
+package com.mhd.coinpaprika.list
+
+import androidx.fragment.app.Fragment
+import com.mhd.coinpaprika.R
+import com.mhd.coinpaprika.databinding.FragmentListBinding
+
+class CoinListFragment : Fragment(R.layout.fragment_list) {
+
+    private var _binding: FragmentListBinding? = null
+    private val binding: FragmentListBinding
+        get() = _binding!!
+}
